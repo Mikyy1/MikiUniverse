d26be84e58493ab20f0eaf9444a2690ef7851bc6
Mikyy1 <71866759+Mikyy1@users.noreply.github.com>
2026-07-08 01:56:33 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 // EDIT ANGKA & LINK VIDEO TIKTOK DI SINI
 const recentTikTokData = {
   username: "@mikyzell",
-  videoUrl: "https://vt.tiktok.com/ZSx4gRaor/",
+  videoUrl: "https://vt.tiktok.com/ZSCcDoFD6/",
   // Kalau mau thumbnail asli TikTok, paste link gambar thumbnail di sini.
   // TikTok tidak selalu mengizinkan ambil thumbnail otomatis dari HTML biasa, karena ya tentu saja hidup harus dipersulit.
   thumbnailUrl: "assets/tiktok-thumb.jpg",
   caption: "Recent upload dari TikTok admin.",
-  views: "12.4K",
-  comments: "328",
-  shares: "97",
+  views: "1,6k",
+  comments: "6",
+  shares: "7",
   uploaded: "Recently uploaded"
 };
